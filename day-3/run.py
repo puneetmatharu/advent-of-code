0582602f0104e0cf876becc3b0874c5a227dc909
Puneet Matharu <puneet.matharu@arm.com>
2023-12-04 10:59:33 +0000

    Condense day 3 pt.1 into a single line. Hehehe (or should I say hohoho).

--- day-3/run.py
+++ day-3/run.py
@@ -8,12 +8,11 @@ def load(fpath: Path) -> list[str]:
 
 
 def part1(fpath: Path):
-    grid = np.pad(np.array([list(l) for l in load(fpath)], dtype=str), 1, constant_values=".")
-    return sum([v for (i, s, e, v) in [[i, *m.span(), int(m.group(0))] for (i, line) in enumerate(load(fpath)) for m in re.finditer(r"(\d+)", line)] if not all([c in "0123456789." for c in [*grid[i, s:e+2], *grid[i+1, [s, e+1]], *grid[i+2, s:e+2]]])])
+    return sum([v for (i, s, e, v) in [[i, *m.span(), int(m.group(0))] for (i, line) in enumerate(load(fpath)) for m in re.finditer(r"(\d+)", line)] if (grid := np.pad(np.array([list(l) for l in load(fpath)], dtype=str), 1, constant_values=".")).size > 0 if not all([c in "0123456789." for c in [*grid[i, s:e + 2], *grid[i + 1, [s, e + 1]], *grid[i + 2, s:e + 2]]])])
 
 
 def part2(fpath: Path):
-    return sum([(n[0]*n[1] if len(n) == 2 else 0) for n in [[v for (iv, s, e, v) in [[i, m.span()[0], m.span()[1], int(m.group(0))] for (i, line) in enumerate(load(fpath)) for m in re.finditer(r"(\d+)", line)] if (ia, ja) in ((iv, s-1), (iv, e), *[(iv-1, j) for j in range(s-1, e+1)], *[(iv+1, j) for j in range(s-1, e+1)])] for (ia, ja) in [[i, m.span()[0]] for (i, line) in enumerate(load(fpath)) for m in re.finditer(r"(\*)", line)]]])
+    return sum([(n[0] * n[1] if len(n) == 2 else 0) for n in [[v for (iv, s, e, v) in [[i, m.span()[0], m.span()[1], int(m.group(0))] for (i, line) in enumerate(load(fpath)) for m in re.finditer(r"(\d+)", line)] if (ia, ja) in ((iv, s - 1), (iv, e), *[(iv - 1, j) for j in range(s - 1, e + 1)], *[(iv + 1, j) for j in range(s - 1, e + 1)])] for (ia, ja) in [[i, m.span()[0]] for (i, line) in enumerate(load(fpath)) for m in re.finditer(r"(\*)", line)]]])
 
 
 if __name__ == "__main__":
